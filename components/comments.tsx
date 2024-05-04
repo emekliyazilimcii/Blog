bf2b5fdbc4ad50b76ab8b5b4d40a910893af6c96
emekliyazilimcii <emekliyazilimci@tutamail.com>
2024-05-04 10:13:28 +0200

    Add commenting

---
+++ components/comments.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import Giscus from '@giscus/react';
+
+const Comments = () => {
+	return (
+		<Giscus
+			repo="emekliyazilimcii/blog"
+			repoId="R_kgDOL290zQ"
+			category="Announcements"
+			categoryId="DIC_kwDOL290zc4CfIa9"
+			mapping="pathname"
+			reactionsEnabled="1"
+			emitMetadata="0"
+			inputPosition="top"
+			theme="preferred_color_scheme"
+			lang="en"
+			loading="lazy"
+		/>
+	);
+};
+
+export default Comments;
