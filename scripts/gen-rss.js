dedf0c46ccb104f1cacb045e14e986dedaf8e7e7
emekliyazilimcii <emekliyazilimci@tutamail.com>
2024-05-05 08:45:56 +0200

    First Post template

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require("gray-matter");
 
 async function generate() {
   const feed = new RSS({
-    title: "Your Name",
-    site_url: "https://yoursite.com",
-    feed_url: "https://yoursite.com/feed.xml",
+    title: "Emekli Yazılımcı",
+    site_url: "https://emekliyazilimci.com",
+    feed_url: "https://emekliyazilimci.com/feed.xml",
   });
 
   const posts = await fs.readdir(path.join(__dirname, "..", "pages", "posts"));
